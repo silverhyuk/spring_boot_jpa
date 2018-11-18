84d16dda2b549dddae311add021062f82f5c3f8e
silverhyuk84 <silverhyuk84@gmail.com>
2018-11-18 20:16:16 +0900

    springDataJpa 활용1

---
+++ src/test/java/com/silverhyuk/spring_boot_jpa/repository/PostRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.silverhyuk.spring_boot_jpa.repository;
+
+import com.silverhyuk.spring_boot_jpa.entity.parentChild.Post;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @Rollback(false)
+    public void crudRepository() {
+        //Given
+        Post post = new Post();
+        post.setTitle("hello spring boot common");
+        assertThat(post.getId()).isNull();
+
+        // When
+        Post newPost = postRepository.save(post);
+
+        // Then
+        assertThat(newPost.getId()).isNotNull();
+
+        // When
+        List<Post> posts = postRepository.findAll();
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts).contains(newPost);
+
+        // When
+        Page<Post> page = postRepository.findAll(PageRequest.of(0, 10));
+        assertThat(page.getTotalElements()).isEqualTo(1);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getSize()).isEqualTo(10);
+        assertThat(page.getNumberOfElements()).isEqualTo(1);
+
+        //When
+        Page<Post> spring = postRepository.findByTitleContains("spring", PageRequest.of(0, 10));
+        assertThat(spring.getTotalElements()).isEqualTo(1);
+        assertThat(spring.getNumber()).isEqualTo(0);
+        assertThat(spring.getSize()).isEqualTo(10);
+        assertThat(spring.getNumberOfElements()).isEqualTo(1);
+
+        //When
+        long springCount = postRepository.countByTitleContains("spring");
+        //Then
+        assertThat(springCount).isEqualTo(1);
+    }
+}
